949ff274a03e4f7997758a6e4317002f9524229d
Andrey Chesnokov <kwampikov@yandex.ru>
2026-07-18 12:25:40 +0300

    tmp

--- api/service/auth/models.go
+++ api/service/auth/models.go
@@ -5,11 +5,6 @@ import (
 	"time"
 )
 
-type Device struct {
-	DeviceName string
-	IPAddress  string
-}
-
 type RegisterRequest struct {
 	Login       string `json:"login"`
 	Password    string `json:"password"`
